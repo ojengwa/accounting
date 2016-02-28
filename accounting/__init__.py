e89c3739cf48e59dc345bbf7640596487639fe78
Bernard Ojengwa <bernardojengwa@gmail.com>
2016-02-28 22:41:36 +0100

    global expose package callabes

--- accounting/__init__.py
+++ accounting/__init__.py
@@ -22,5 +22,17 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
+from accounting.accounting import Accounting
+from accounting.utils import (is_num, is_str, check_type, check_precision)
+
 
 __version__ = '0.0.1'
+
+__all__ = [
+    'Accounting',
+    '__version__',
+    'is_num',
+    'is_str',
+    'check_precision',
+    'check_type'
+]
