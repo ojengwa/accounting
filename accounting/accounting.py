430caf4ee712b80f35ffc10f00d48fe8f78f3beb
Bernard Ojengwa <bernardojengwa@gmail.com>
2016-02-28 22:23:56 +0100

    create as_money method

--- accounting/accounting.py
+++ accounting/accounting.py
@@ -112,7 +112,7 @@ class Accounting(object):
         val = (lambda num: base if is_num(num) else num)(val)
         return val
 
-    def parse(self, value, decimal='.'):
+    def parse(self, value, decimal=None):
         """
         Summary.
 
@@ -176,7 +176,7 @@ class Accounting(object):
         power = round(self.parse(value) * power) / power
         return '{0} {1}.{2}f'.format(value, precision, precision)
 
-    def format(self, number, precision, thousand, decimal):
+    def format(self, number, **kwargs):
         """Format a given number.
 
         Format a number, with comma-separated thousands and
@@ -196,16 +196,15 @@ class Accounting(object):
         """
         # Resursively format lists
         if check_type(number, 'list'):
-            return map(lambda val: self.format(val, precision, thousand,
-                                               decimal), number)
+            return map(lambda val: self.format(val, **kwargs))
         # Clean up number
         number = self.parse(number)
 
         # Build options object from second param (if object) or all params,
         # extending defaults
-        if check_type(precision, 'dict'):
-            options = (self.settings['number'].update(precision))
-        print(options)
+        if check_type(kwargs, 'dict'):
+            options = (self.settings['number'].update(kwargs))
+
         # Clean up precision
         precision = self._check_precision(options['precision'])
         negative = (lambda num: "-" if num < 0 else "")(number)
@@ -222,3 +221,53 @@ class Accounting(object):
             .split('.')[1] if val else '')(precision)
 
         return num
+
+    def as_money(self, number, **options):
+        """Format a number into currency.
+
+        Usage: accounting.formatMoney(number, symbol, precision, thousandsSep,
+                                      decimalSep, format)
+        defaults: (0, "$", 2, ",", ".", "%s%v")
+        Localise by overriding the symbol, precision,
+        thousand / decimal separators and format
+        Second param can be an object matching `settings.currency`
+        which is the easiest way.
+
+        Args:
+            number (TYPE): Description
+            precision (TYPE): Description
+            thousand (TYPE): Description
+            decimal (TYPE): Description
+
+        Returns:
+            name (TYPE): Description
+        """
+        # Resursively format arrays
+        if isinstance(number, list):
+            return map(lambda val: self.as_money(val, **options))
+
+        # Clean up number
+        decimal = options.get('decimal')
+        number = self.parse(number, decimal)
+
+        # Build options object from second param (if object) or all params,
+        # extending defaults
+        if check_type(options, 'dict'):
+            options = (self.settings['currency'].update(options))
+
+        # Check format (returns object with pos, neg and zero)
+        formats = self._check_currency_format(options['format'])
+
+        # Choose which format to use for this value
+        use_format = (lambda num: formats['pos'] if num > 0 else formats[
+                      'neg'] if num < 0 else formats['zero'])(number)
+        precision = self._check_precision(number, options['precision'])
+        thousands = options['thousand']
+        decimal = options['decimal']
+        formater = self.format(abs(number), precision, thousands, decimal)
+
+        # Return with currency symbol added
+        amount = use_format.replace(
+            '%s', options['symbol']).replace('%v', formater)
+
+        return amount
